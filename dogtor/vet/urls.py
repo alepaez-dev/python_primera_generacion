c6533b234ae7f69256165315264dc1dc85ce5ffd
= <=>
2021-03-17 21:08:36 -0700

    Agregando pets

--- dogtor/vet/urls.py
+++ dogtor/vet/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import list_pet_owners
+from .views import list_pet_owners, Test, Owners, OwnersList, OwnersDetail, PetsList, PetsDetail
 
 urlpatterns = [
-  path("owners/", list_pet_owners)
+  path("owners/", OwnersList.as_view()),
+  path("pets/", PetsList.as_view()),
+  path("owners/<int:pk>/", OwnersDetail.as_view()),
+  path("pets/<int:id>/", PetsDetail.as_view()),
+  path("test/", Test.as_view()),
 ]
--- dogtor/vet/views.py
+++ dogtor/vet/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.views.generic import View, TemplateView, ListView, DetailView
 
-from .models import PetOwner
+from .models import PetOwner, Pet
 
 # Create your views here.
 def list_pet_owners(request):
@@ -12,3 +13,56 @@ def list_pet_owners(request):
 
     template = loader.get_template("vet/owners/list.html")
     return HttpResponse(template.render(context, request))
+
+class Owners(View):
+    def get(self, request):
+        owners = PetOwner.objects.all()
+        context = {"owners": owners}
+
+        template = loader.get_template("vet/owners/list.html")
+        return HttpResponse(template.render(context, request))
+
+# class OwnersList(TemplateView):
+#     template_name = "vet/owners/list.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['owners'] = PetOwner.objects.all()
+#         return context
+
+class OwnersList(ListView):
+    model = PetOwner
+    template_name = "vet/owners/list.html"
+    context_object_name = "owners"
+
+def test(request):
+    print(request.__dict__)
+    return HttpResponse("Hello world!!")
+
+class OwnersDetail(DetailView):
+    model = PetOwner
+    template_name = "vet/owners/detail.html"
+    context_object_name = "owner"
+
+#clases
+class Test(View):
+
+    def get(self, request):
+        return HttpResponse("Hello world from class view!!")
+
+###Pet
+class PetsList(TemplateView):
+    template_name = "vet/pets/list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pets'] = Pet.objects.all()
+        return context
+
+class PetsDetail(View):
+    def get(self, request, id):
+        pet = Pet.objects.get(id=id)
+        context = {"pet": pet}
+        template = loader.get_template("vet/pets/detail.html")
+        return HttpResponse(template.render(context, request))
+        
